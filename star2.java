3b9f908595b57927eff60f56e9f1b3bac6927a08
chaithraraju <145969085+chaithraraju@users.noreply.github.com>
2023-09-28 18:40:31 +0530

    Add files via upload

---
+++ star2.java
@@ -0,0 +1,16 @@
+package com.acaders.star;
+
+public class star2 
+{
+public static void main(String[] args)
+{
+	for(int i=1;i<=10;i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+  }
+}
