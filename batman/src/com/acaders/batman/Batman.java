8b228280474d2dbeef80aef6dc3b4c63d572f854
chaithraraju <145969085+chaithraraju@users.noreply.github.com>
2023-09-28 16:31:41 +0530

    Add files via upload

---
+++ batman/src/com/acaders/batman/Batman.java
@@ -0,0 +1,189 @@
+package com.acaders.batman;
+
+public class Batman 
+{
+public static void main(String[] args)
+{
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=5;j>=i;j--)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print("*");
+		}
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print("*");
+		}
+		for(int j=i;j<=4;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=4;j>=i;j--)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print("*");
+		}
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print("*");
+		}
+		System.out.println(" ");
+	}
+	for(int i=1;i<=5;i++)
+	{
+	for(int j=1;j<=5;j++)
+	{
+		System.out.print("*");
+	  }
+	for(int j=1;j<=i;j++)
+	{
+		System.out.print(" ");
+	}
+	for(int j=i;j<=5;j++)
+	{
+		System.out.print("*");
+	}
+	for(int j=1;j<=5;j++)
+	{
+		System.out.print(" ");
+	  }
+	for(int j=1;j<=5;j++)
+	{
+		System.out.print(" ");
+	  }
+	for(int j=5;j>=i;j--)
+	{
+		System.out.print("*");
+	}
+	for(int j=1;j<=i;j++)
+	{
+		System.out.print(" ");
+	}
+	for(int j=1;j<=4;j++)
+	{
+		System.out.print("*");
+	}
+	    System.out.println();
+    }
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print("*");
+		}
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print(" ");
+		}for(int j=1;j<=6;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	for(int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print("*");
+		}
+		for(int j=5;j>=i;j--)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print("*");
+		}
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print("*");
+		}
+		for(int j=i;j<=4;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=4;j>=i;j--)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print("*");
+		}
+		for(int j=1;j<=i;j++)
+		{
+			System.out.print("*");
+		}
+		for(int j=i;j<=5;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=4;j++)
+		{
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+	for (int i=1;i<=5;i++)
+	{
+		for(int j=1;j<=i;j++)
+		{
+	System.out.print(" ");
+    }
+		for(int j=i;j<=5;j++)
+		{
+			System.out.print("*");
+		}
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=1;j<=5;j++)
+		{
+			System.out.print(" ");
+		}
+		for(int j=5;j>=i;j--)
+		{
+			System.out.print("*");
+		}
+		
+     System.out.println();
+   }
+  }
+	 }
+ 
