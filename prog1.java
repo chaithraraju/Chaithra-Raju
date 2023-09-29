157a0c546623b2b6959bd9dfd78794cc65b762da
chaithraraju <145969085+chaithraraju@users.noreply.github.com>
2023-09-29 08:25:43 +0530

    Add files via upload

---
+++ prog1.java
@@ -0,0 +1,22 @@
+package com.acaders.array;
+
+public class prog1 
+{
+
+	public static void main(String[] args)
+	{
+		int a[]=new int[5];
+		a[0]=60;
+		a[1]=45;
+		a[2]=30;
+		a[3]=22;
+		a[4]=26;
+		
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		System.out.println(a[3]);
+		System.out.println(a[4]);
+	}
+
+}
