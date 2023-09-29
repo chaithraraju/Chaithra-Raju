6cc847cba248f4ad69d5a640ff484066ee8db97a
chaithraraju <145969085+chaithraraju@users.noreply.github.com>
2023-09-29 09:01:53 +0530

    Add files via upload

---
+++ example1.java
@@ -0,0 +1,24 @@
+package demo.com.acaders;
+
+
+class demo1
+{
+	void add(float x,float b)
+	{
+		System.out.println(x+b);
+	}
+	void ad(double a, double b)
+	{
+		System.out.println(a+b);
+	}
+}
+ class example1 
+{
+public static void main(String []args)
+{
+	float x=20.5f , y=300.5f;
+	
+	demo1 d1=new demo1();
+	d1.add(x,y);
+}
+}
