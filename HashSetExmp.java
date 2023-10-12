4fe1f1a3a8235b94ea668a300018586363ed13c6
chaithraraju <145969085+chaithraraju@users.noreply.github.com>
2023-10-12 08:55:49 +0530

    Add files via upload

---
+++ HashSetExmp.java
@@ -0,0 +1,19 @@
+package collectionpractice;
+
+import java.util.HashSet;
+
+public class HashSetExmp {
+
+	public static void main(String[] args) {
+		HashSet hs = new HashSet();
+		hs.add(50);
+		hs.add(150);
+		hs.add(25);
+		hs.add(75);
+		hs.add(125);
+		hs.add(100);
+		hs.add(175);
+		System.out.println(hs);
+	}
+
+}
