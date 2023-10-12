14e236d9adf8e99e5b0f1f7759e023cb91d817f7
chaithraraju <145969085+chaithraraju@users.noreply.github.com>
2023-10-12 08:56:54 +0530

    Add files via upload

---
+++ priorityQueueExmp.java
@@ -0,0 +1,20 @@
+package collectionpractice;
+
+import java.util.PriorityQueue;
+
+public class priorityQueueExmp 
+{
+public static void main(String[] args)
+{
+PriorityQueue pq = new PriorityQueue();
+pq.add(30);
+pq.add(70);
+pq.add(20);
+pq.add(10);
+pq.add(90);
+pq.add(60);
+pq.add(80);
+pq.add(10);
+System.out.println(pq);
+}
+}
